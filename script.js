e7f5975066ccf270cadf1136860afbe97086b553
Joy <joyceassis96@gmail.com>
2022-08-14 20:06:41 -0300

    contador

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-let input = document.querySelector('.input')
+let one = document.querySelector(".addOne");
+
+let count = 0;
+
+function Add(){
+    count = count + 1;
+    one.textContent = count;
+}
+function Less(){
+    count = count - 1;
+    one.textContent = count;
+
+}
+
